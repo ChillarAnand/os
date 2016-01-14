0c1555ffbe3b61daa2695f89d6356431eded8ea4
chillaranand <anand21nanda@gmail.com>
2016-01-14 09:42:44 +0530

    Update bootstrap script

--- start.sh
+++ start.sh
@@ -3,5 +3,12 @@
 cd
 mkdir -p projects/ubuntu
 cd projects/ubuntu
+
 sudo apt-get install --yes git
+git config --global user.name 'chillaranand'
 git clone https://github.com/ChillarAnand/os.git
+
+
+# salt
+./salt/start/master.sh
+./salt/start/minion.sh
