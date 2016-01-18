f5e8c7fe1a494b30ec6c8bd2c4ad95b7bc5d388a
chillaranand <anand21nanda@gmail.com>
2016-01-18 12:02:31 +0530

    Install byobu, zsh  first

--- start.sh
+++ start.sh
@@ -1,11 +1,21 @@
 #! /bin/sh
 
 cd
+
+sudo apt-get install --yes git
+sudo apt-get install --yes byobu
+sudo apt-get install --yes zsh
+
+git config --global user.name 'chillaranand'
+git clone https://github.com/robbyrussell/oh-my-zsh.git .oh-my-zsh
+git clone https://github.com/ChillarAnand/.custom-zsh.git .custom-zsh
+ln -s .custom-zsh/zshrc .zshrc
+chsh -s /usr/bin/zsh anand
+
+
 mkdir -p projects/ubuntu
 cd projects/ubuntu
 
-sudo apt-get install --yes git
-git config --global user.name 'chillaranand'
 git clone https://github.com/ChillarAnand/os.git
 cd os
 git pull origin master
