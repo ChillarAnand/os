25ff476ae63d0c1a8ed5a8e3d36f5709082fcb9f
chillaranand <anand21nanda@gmail.com>
2016-01-18 17:37:33 +0530

    Update space2cctl

--- start.sh
+++ start.sh
@@ -27,21 +27,15 @@ cd projects/ubuntu
 # ./s2cctl start
 # cd ..
 
+
 # install xcape
 sudo apt-get install git gcc make pkg-config libx11-dev libxtst-dev libxi-dev
 git clone https://github.com/alols/xcape.git
 cd xcape
 make
 sudo make install
-
-spare_modifier="Hyper_L"
-xmodmap -e "keycode 65 = $spare_modifier"
-xmodmap -e "remove mod4 = $spare_modifier" # hyper_l is mod4 by default
-xmodmap -e "add Control = $spare_modifier"
-xmodmap -e "keycode any = space"
-xcape -e "$spare_modifier=space"
-
 cd ..
+./space2ctrl.sh
 
 
 # salt setup
