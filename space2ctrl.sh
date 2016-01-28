3164f07ac4f1d756c77cacd9ead14d8928350192
anand21nanda@gmail.com <anand21nanda@gmail.com>
2016-01-28 14:21:56 +0530

    Update space2ctrl

--- space2ctrl.sh
+++ space2ctrl.sh
@@ -1,3 +1,24 @@
+cd ~/projects/ubuntu
+
+
+# install xcape
+if [ ! -f xcape/xcape ]; then
+    sudo apt-get install --yes git gcc make pkg-config libx11-dev libxtst-dev libxi-dev
+    git clone https://github.com/alols/xcape.git
+    cd xcape
+    make
+    sudo make install
+    cd ..
+    echo "xcape installed"
+fi
+
+
+# reset kbd, xcape
+setxkbmap -layout us
+ps -ef | grep "[x]cape" | awk '{print $2}' | xargs kill -9
+
+
+# use space as space/ctrl
 spare_modifier="Hyper_L"
 xmodmap -e "keycode 65 = $spare_modifier"
 xmodmap -e "remove mod4 = $spare_modifier" # hyper_l is mod4 by default
