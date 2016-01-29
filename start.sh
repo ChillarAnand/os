199c54209a1c08b943a2565c6b27e080c672a9b1
chillaranand <anand21nanda@gmail.com>
2016-01-29 14:30:24 +0530

    Update apt-fast ppa

--- start.sh
+++ start.sh
@@ -16,7 +16,7 @@ cd
 if [ ! -f /usr/sbin/apt-fast ]; then
     echo "Installing apt-fast..."
     # apt-fast
-    sudo add-apt-repository --yes ppa:apt-fast/stable > /tmp/foo
+    sudo add-apt-repository --yes ppa:saiarcot895/myppa > /tmp/foo
     sudo apt-get -qq update
     sudo apt-get -qq install --yes apt-fast
     # sudo dpkg-reconfigure apt-fast  # configure
