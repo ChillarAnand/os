31a02f3c5598f7b2864e5bb2164cdcd72c0a11fc
chillaranand <anand21nanda@gmail.com>
2016-02-09 17:20:33 +0530

    Update cron jobs

--- bin/startup.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/sh
-
-nohup /usr/bin/synapse -s &
-nohup /usr/bin/clementine &
-nohup /usr/bin/emacs &
-nohup /usr/bin/google-chrome &
-nohup gnome-terminal -x sh -c "/usr/bin/byobu -S boot-time;"
